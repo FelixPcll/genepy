5fe7d47659441070d984b67c7d3e1a0e9553ea34
FelixPcll <felix@df.ufscar.br>
2018-06-27 01:14:27 -0300

    Added breed function on Individual

--- individual.py
+++ individual.py
@@ -13,3 +13,24 @@ class Individual:
         if not self.par:
             self.par = [rd.random() for i in range(10)]
     
+
+    def breed(self, other, mash=3, auto_add=False):
+        
+        dna1 = self.par
+        dna2 = other.par
+
+        dna3 = []
+        dna4 = []
+
+        #nodes = [(i+1)*int(len(dna1)/mash) for i in range(mash)]
+        #for n, node in enumerate(nodes):
+        #    if n % 0 == 0:
+        #        dna3.extend(dna1[])
+
+        node = [3,7]
+
+        dna3.extend( dna1[:node[0]] + dna2[node[0]:node[1]] + dna1[node[1]:] )
+        dna4.extend( dna2[:node[0]] + dna1[node[0]:node[1]] + dna2[node[1]:] )
+
+        if not auto_add:
+            return dna3, dna4
